3f47b4551272279dfa3532542f7ef9c0ac8bdbc7
kasirimo <kasirimod@gmail.com>
2026-09-07 21:19:37 +0300

    Final vercel test

--- api/contact.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import contact from "../controllers/contactcontroller.js";
-
-const contactRoute = express.Router();
-
-contactRoute.post("/contact", contact);
-
-export default contactRoute;
