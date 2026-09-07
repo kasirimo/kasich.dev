81d60ff89b2f78824f7418bc984a3bd9c47d9c9b
kasirimo <kasirimod@gmail.com>
2026-09-07 17:21:29 +0300

    Modified build 2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 import contactRoute from "./routes/contact.js";
 import contact from "./controllers/contactcontroller.js";
@@ -12,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.use("/contact", contactRoute);
 
