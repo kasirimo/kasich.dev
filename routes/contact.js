76e3a00c753ef29b568beaa9da0042c1bbf57e05
kasirimo <kasirimod@gmail.com>
2026-09-07 19:31:33 +0300

    Modified build 9

--- routes/contact.js
+++ routes/contact.js
@@ -4,5 +4,8 @@ import contact from "../controllers/contactcontroller.js";
 const contactRoute = express.Router();
 
 contactRoute.post("/", contact);
+contactRoute.get("/", (req,res) => {
+    res.send("Hello");
+})
 
 export default contactRoute;
