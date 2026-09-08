f4c2a6d47f1ddaaab93f77c7fc10be41549b73f8
kasirimo <kasirimod@gmail.com>
2026-09-08 13:12:27 +0300

    Removed slash from origin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "https://kasich-dev.vercel.app/",
+    origin: "https://kasich-dev.vercel.app",
     credentials: true
 }));
 
