2df2a29b1b7dd79599bdfb8219165904c1a4ba09
kasirimo <kasirimod@gmail.com>
2026-09-08 12:53:12 +0300

    Changed the cors origin method

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "https://kasich-dev.vercel.app",
-    credentials: true
+    origin: "https://kasich-dev.vercel.app/"    
 }));
 
 app.use(express.json());
