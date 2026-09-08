4f37ce8dc3f2cc381a724e2e229cedb9b3624393
kasirimo <kasirimod@gmail.com>
2026-09-08 11:28:03 +0300

    Testing api route

--- routes/contact.js
+++ routes/contact.js
@@ -4,5 +4,8 @@ import contact from "../controllers/contactcontroller.js";
 const router = express.Router();
 
 router.post("/", contact);
+router.get("/", (req,res) => {
+    res.send("Hello");
+})
 
 export default router;
