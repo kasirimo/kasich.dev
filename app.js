de686edcd0c865226c7393cec439e00a814f2a19
kasirimo <kasirimod@gmail.com>
2026-09-08 11:39:54 +0300

    Saving a few changes before vercel to render

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors());
+app.use(cors({
+    origin: "https://kasich-dev.vercel.app",
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
