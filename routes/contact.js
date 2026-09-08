9873ca45c5cb1bf0e49c3bdd588f7ddc5afcfaeb
kasirimo <kasirimod@gmail.com>
2026-09-08 12:57:05 +0300

    Removed contact get route

--- routes/contact.js
+++ routes/contact.js
@@ -4,8 +4,5 @@ import contact from "../controllers/contactcontroller.js";
 const router = express.Router();
 
 router.post("/", contact);
-router.get("/", (req,res) => {
-    res.send("Hello");
-})
 
 export default router;
