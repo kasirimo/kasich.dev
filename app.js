fd9d7ab2f98abea5a15334b54d5fafd52a200a16
kasirimo <kasirimod@gmail.com>
2026-09-08 11:13:43 +0300

    Another deployment test 1 of the day

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.use("/api/contact", contactRoute);
 
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "./public", "index.html"));
 })
 
 export default app;
