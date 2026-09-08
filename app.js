fb729a8fdc235ed5fdfeeca091fd0a21eef108c5
kasirimo <kasirimod@gmail.com>
2026-09-08 13:08:02 +0300

    Changed fetch on resend

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "https://kasich-dev.vercel.app"    
+    origin: "https://kasich-dev.vercel.app/",
+    credentials: true
 }));
 
 app.use(express.json());
